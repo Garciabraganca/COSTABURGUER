efa6fcafba85e8fd6a8ecdab9f67f0ed88b629ec
Grupo Garcia Bragança <grupogarciabraganca@gmail.com>
2025-12-14 14:40:28 -0300

    Force root alias for @ imports

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 const nextConfig = {
   reactStrictMode: true,
   webpack(config) {
-    config.resolve.alias['@'] = config.resolve.alias['@'] || path.resolve(__dirname);
+    config.resolve.alias['@'] = path.resolve(__dirname);
     return config;
   },
 };
