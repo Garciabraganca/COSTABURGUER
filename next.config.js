23e79961167106f1d8857ceea7e8c409022ef2d7
Grupo Garcia Bragança <grupogarciabraganca@gmail.com>
2025-12-19 13:19:25 -0300

    Add floating final burger preview

--- next.config.js
+++ next.config.js
@@ -3,13 +3,6 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  api: {
-    // Permite uploads maiores (ex.: fotos enviadas de iOS/Android)
-    bodyParser: {
-      sizeLimit: '15mb',
-    },
-    responseLimit: '15mb',
-  },
   webpack: (config) => {
     config.resolve.alias['@'] = path.resolve(__dirname);
     return config;
